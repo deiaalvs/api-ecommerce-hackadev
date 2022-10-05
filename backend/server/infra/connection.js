7fec1f8ab084c89bb5bd0811c0f420fa9f21a231
Andreia Costa <andreia_a.c@msn.com>
2022-10-05 13:25:37 -0300

    configs for heroku deploy

--- backend/server/infra/connection.js
+++ backend/server/infra/connection.js
@@ -1,14 +1,14 @@
 const pgp = require('pg-promise')();
 const db = pgp (
     {
-        user: 'postgres',
+        user: 'dksvejnsthhcwh',
         /* Inform below your database adm's password */
-        password: '',
-        host: 'localhost',
+        password: '6b249cdb5e760fd05b955156a8729917ce574765cd3f0bfafbd378b135a2a72a',
+        host: 'ec2-34-231-221-151.compute-1.amazonaws.com',
         /* Inform below your database's port */
         port: '5432',
         /* Inform below your database's name */
-        database: ''
+        database: 'd4421tca4c4mtk'
     }
 );
 

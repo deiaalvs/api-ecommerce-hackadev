acefe08e6eccc87e819cb974fa24f407d7f096ec
Andreia Costa <aandreia_a.c@msn.com>
2022-10-05 22:43:12 -0300

    configs for heroku deploy v12

--- server/infra/connection.js
+++ server/infra/connection.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg');
-
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
+client.connect();
 
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -15,12 +15,28 @@ const pool = new Pool({
   }
 });
 
-client.connect();
-
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
+  if (err) throw ` O erro é no client query ${err}`;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
   client.end();
 });
+
+const express = require('express');
+const router = express.Router();
+
+const purchaseStatusService = require('../service/purchaseStatusService');
+
+router.get('/purchaseStatus', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM PurchaseStatus');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('/purchaseStatus', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send(`O erro é no pool get ${err}`);
+  }
+})
